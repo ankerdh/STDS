d2db95894e2b664ba8c4d247576f963034b36f1a
dha <dave.anker@gmail.com>
2017-09-02 09:55:03 +1000

    +incidents

--- incidents.R
+++ incidents.R
@@ -1,6 +1,6 @@
 library(httr)
 library(jsonlite)
-result <- GET("https://api.transport.nsw.gov.au/v1/live/hazards/majorevent/all", 
+result <- GET("https://api.transport.nsw.gov.au/v1/live/hazards/incident/all", 
               verbose(), 
               encode="json", 
               add_headers(Authorization = "apikey 5I42m6prdSGmGohy1mMub1SHNGm6hxWjrg0a", lastPublished=""))
