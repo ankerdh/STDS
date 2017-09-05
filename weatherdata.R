04e5eaa87188d7e11e7e008500137c96c3455df3
jradhakrishnan84 <jayakrishnan.radhakrishnan@student.uts.edu.au>
2017-09-06 00:08:15 +1000

    add capabilities to fetch forecast details

--- weatherdata.R
+++ weatherdata.R
@@ -13,10 +13,18 @@ location_key <- location_clean[1,]$Key
 
 #get the current weather data for the location from Accuweather current conditions API
 
-getcurrent <- GET("http://dataservice.accuweather.com/currentconditions/v1/54968_PC?apikey=HNyjZnlRykmDQLvWoyCOc460vyjPg9dJ")
+getcurrent <- GET("http://dataservice.accuweather.com/currentconditions/v1/54968_PC?apikey=HNyjZnlRykmDQLvWoyCOc460vyjPg9dJ&details=true")
 names(getcurrent)
 getcurrent$status_code
 content(getcurrent)
 current_raw <- rawToChar(getcurrent$content)
 current_clean <-fromJSON(current_raw,simplifyDataFrame = TRUE)
-  
+
+#get forecast weather data for next 1 hour
+getforecast<- GET("http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/54968_PC?apikey=HNyjZnlRykmDQLvWoyCOc460vyjPg9dJ&details=true")
+getforecast$status_code
+content(getforecast)
+forecast_raw <-rawToChar(getforecast$content)
+class(forecast_raw)
+forecast_clean <-fromJSON(forecast_raw,simplifyDataFrame = TRUE)
+
